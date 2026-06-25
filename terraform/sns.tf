88b162bddccd4fa2fddd0290ec3234330269f3ca
Uday <forteciipher@gmail.com>
2026-06-25 14:47:43 +0530

    Fix CodePipeline SNS event notifications
    Fix EventBridge rule and SNS delivery so pipeline execution events trigger email alerts.
    
    Signed-off-by: Uday <forteciipher@gmail.com>

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_sns_topic" "pipeline_alerts" {
   name = "pipeline-alerts-${var.environment}"
 }
@@ -12,20 +14,30 @@ resource "aws_sns_topic_policy" "allow_events" {
   arn = aws_sns_topic.pipeline_alerts.arn
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "events.amazonaws.com" }
-      Action    = "sns:Publish"
-      Resource  = aws_sns_topic.pipeline_alerts.arn
-    }]
+    Statement = [
+      {
+        Sid       = "AllowEventBridgePublish"
+        Effect    = "Allow"
+        Principal = { Service = "events.amazonaws.com" }
+        Action    = "sns:Publish"
+        Resource  = aws_sns_topic.pipeline_alerts.arn
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
   })
 }
 
 resource "aws_cloudwatch_event_rule" "pipeline_events" {
-  name = "pipeline-events-${var.environment}"
+  name        = "pipeline-events-${var.environment}"
+  description = "Capture all CodePipeline execution state changes and send to SNS"
+  state       = "ENABLED"
   event_pattern = jsonencode({
     source      = ["aws.codepipeline"]
-    detail-type = ["CodePipeline Pipeline Execution State Change"]
+    "detail-type" = ["CodePipeline Pipeline Execution State Change"]
     detail = {
       pipeline = [aws_codepipeline.pipeline.name]
     }
@@ -35,4 +47,19 @@ resource "aws_cloudwatch_event_rule" "pipeline_events" {
 resource "aws_cloudwatch_event_target" "to_sns" {
   rule = aws_cloudwatch_event_rule.pipeline_events.name
   arn  = aws_sns_topic.pipeline_alerts.arn
+  input_transformer {
+    input_paths = {
+      pipeline  = "$.detail.pipeline"
+      state     = "$.detail.state"
+      execution = "$.detail.execution-id"
+      time      = "$.time"
+    }
+    input_template = <<-EOT
+      "Pipeline: <pipeline>"
+      "State:    <state>"
+      "Run ID:   <execution>"
+      "Time:     <time>"
+      "Console:  https://console.aws.amazon.com/codesuite/codepipeline/pipelines/<pipeline>/view"
+    EOT
+  }
 }
