b8715842b812678c51abf98d6797c526e8143afd
Uday <forteciipher@gmail.com>
2026-06-25 12:41:42 +0530

    Add CodeBuild project
    Standard Linux image, small compute, logs to CloudWatch. Buildspec comes from the repo, not inline, so build changes are code reviewed.
    
    Signed-off-by: Uday <forteciipher@gmail.com>

---
+++ terraform/codebuild.tf
@@ -0,0 +1,25 @@
+resource "aws_codebuild_project" "build" {
+  name         = "flask-app-build-${var.environment}"
+  service_role = aws_iam_role.codebuild.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "/aws/codebuild/flask-app-${var.environment}"
+    }
+  }
+}
